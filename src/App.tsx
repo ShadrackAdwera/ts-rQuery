a5eb023e5d22aca3a6417eb34d17d39df1ef05b0
ShadrackAdwera <adweshshaddie@gmail.com>
2022-06-08 08:32:40 +0300

    wire up react query and chakra ui theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+import { queryClient } from './reactQuery/react-query';
+
+import Routes from './routes';
+import LoadingSpinner from './shared/Spinner';
+import { theme } from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Routes />
+        <LoadingSpinner />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ChakraProvider>
   );
 }
 
