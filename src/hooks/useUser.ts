b755c59d568f5e4be4259ccd3d264279c745f02a
ShadrackAdwera <adweshshaddie@gmail.com>
2022-06-14 08:23:49 +0300

    call useUser from useAuth hook

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,16 +1,44 @@
 import { AxiosResponse } from 'axios';
+import { useQuery, useQueryClient } from 'react-query';
 
-import { User } from '../shared/types';
+import { User, Roles } from '../shared/types';
+import { queryKeys } from '../shared/queryKeys';
 import { axiosInstance } from '../shared/axios/axiosInstance';
 
-const fetchUser = async (user: User | null): Promise<User | null> => {
+async function fetchUser(
+  user: User | null,
+  signal: AbortSignal | undefined
+): Promise<User | null> {
   if (!user) return null;
-  const { data }: AxiosResponse<{ user: User | null }> = await axiosInstance({
-    url: `/users/${user.id}`,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${user.token}`,
-    },
-  });
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      signal,
+    }
+  );
   return data.user;
+}
+
+interface IUseUser {
+  user: User | null;
+  updateUser: (user: User) => void;
+  clearUser: () => void;
+}
+
+export const useUser = (): IUseUser => {
+  const queryClient = useQueryClient();
+  const user = null;
+  //const { data: user } = useQuery(queryKeys.USER, ({ signal })=>fetchUser(user, signal))
+
+  //called from useAuth
+  const updateUser = (updatedUser: User) => {
+    queryClient.setQueryData(queryKeys.USER, updatedUser);
+  };
+
+  //invalidate queries - called from userAuth
+  const clearUser = () => {
+    queryClient.setQueryData(queryKeys.USER, null);
+  };
+
+  return { user, updateUser, clearUser };
 };
